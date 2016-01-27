b8c1ad5e32ee6f368ab359241131337f85c37096
Daren Schaad and Will Johnson <trashmoldwilliams@gmail.com>
2016-01-27 10:47:21 -0800

    Add basic logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,41 @@
+var wordBank = ["cat", "couch", "computer", "airplane", "programming", "refrigerator", "tunnel", "apricot", "symphany", "obsequious", "beautiful", "runner", "bowling", "pitcher", "global", "brainiac"];
 
+var wordGrabber = function(randomNumber) {
+  var currentWord = wordBank[randomNumber];
+  return currentWord;
+}
 
+var wordSplitter = function(currentWord) {
+  var backEndLetters = currentWord.split("");
+  return backEndLetters;
+}
 
-$(document).ready(function (){
+var letterEncryption = function(backEndLetters) {
+  var frontEndLetters = backEndLetters;
+  for (var i = 0; i < backEndLetters.length; i++) {
+    frontEndLetters[i] = "_";
+    }
+  return frontEndLetters;
+}
 
-});
+var letterGuess = function(currentGuess, backEndLetters, frontEndLetters, incorrectGuessCounter) {
+  var correctGuess = false;
+  for (var i = 0; i < backEndLetters.length; i++) {
+    if (currentGuess === backEndLetters[i]) {
+      frontEndLetters = revealCorrectLetter(i, currentGuess, frontEndLetters);
+      correctGuess = true;
+    }
+  }
+
+  if (correctGuess === true) {
+    return frontEndLetters;
+  } else {
+    incorrectGuessCounter += 1;
+    return incorrectGuessCounter;
+  }
+}
+
+var revealCorrectLetter = function(currentIndex, currentGuess, frontEndLetters) {
+  frontEndLetters[currentIndex] = currentGuess;
+  return frontEndLetters;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,33 +1,38 @@
-describe('Player', function() {
-    it("returns the player's mark", function() {
-      var testPlayer = new Player("X");
-      expect(testPlayer.mark()).to.equal("X");
+describe('wordGrabber', function(randomNumber) {
+    it("will grab a word from the specific index of the wordBank", function() {
+      expect(wordGrabber(3)).to.equal("airplane");
    });
 });
 
-describe('Space', function() {
-    it("returns the player's mark", function() {
-      var testSpace = new Space(1,2);
-      expect(testSpace.x-coordinate).to.equal(1);
-   });
+describe('wordSplitter', function(currentWord) {
+  it("will split the characters of the word string into an array of letters", function() {
+    expect(wordSplitter("airplane")).to.eql(["a", "i", "r", "p", "l", "a", "n", "e"])
+  });
 });
 
-describe('Space', function() {
-    it("returns the player's mark", function() {
-      var testSpace = new Space(1,2);
-      expect(testSpace.y-coordinate).to.equal(2);
-   });
-
-  it("lets a player mark a space", function() {
-      var testPlayer = new Player("X")
-      var testSpace = new Space(1,2);
-      testSpace.markedBy(testPlayer)
-      expect(testSpace.markedBy()).to.equal(testPlayer);
-   });
+describe('letterEncryption', function(backEndLetters) {
+  it("will change all letters in array to underscores", function() {
+    expect(letterEncryption(["a", "i", "r", "p", "l", "a", "n", "e"])).to.eql(["_", "_", "_", "_", "_", "_", "_", "_"])
+  });
 });
 
-describe('Board', function() {
-  it("creates 9 spaces when it is initialized", function() {
-    // You write the rest!
-   });
+describe('letterGuess', function(currentGuess, backEndLetters, frontEndLetters, incorrectGuessCounter) {
+  it("will compare users guess to the current word's letters", function() {
+    expect(letterGuess("a", ["a", "i", "r", "p", "l", "a", "n", "e"], ["_", "_", "_", "_", "_", "_", "_", "_"], 0)).to.eql(["a", "_", "_", "_", "_", "a", "_", "_"])
+  });
+  it("will add 1 to the incorrectGuessCounter when currentGuess does not equal any letters in the backEndLetters", function() {
+    expect(letterGuess("j", ["a", "i", "r", "p", "l", "a", "n", "e"], ["_", "_", "_", "_", "_", "_", "_", "_"], 0)).to.equal(1)
+  });
+});
+
+describe('revealCorrectLetter', function(currentIndex, currentGuess, frontEndLetters) {
+  it("will replace underscore in frontEndLetters with a correct currentGuess letter", function() {
+    expect(revealCorrectLetter(0, "a", ["_", "_", "_", "_", "_", "_", "_", "_"])).to.eql(["a", "_", "_", "_", "_", "_", "_", "_"])
+  });
+});
+
+describe('winLoseCondition', function (incorrectGuessCounter, frontEndLetters) {
+  it("will end the game if player has guessed wrong 8 times", function() {
+    expect(8, ["_", "b", "_"]).to.equal("lose")
+  });
 });
